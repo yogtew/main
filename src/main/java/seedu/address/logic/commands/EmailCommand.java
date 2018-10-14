49369dd79aa40539866cbac66e8bbda586e59ff0
preethid <e0203237@u.nus.edu>
2018-10-14 14:59:45 +0800

    Accept email command

--- src/main/java/seedu/address/logic/commands/EmailCommand.java
+++ src/main/java/seedu/address/logic/commands/EmailCommand.java
@@ -1,17 +1,16 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
-
-import java.util.List;
-
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.Email;
-import seedu.address.ui.BrowserPanel;
+import seedu.address.model.person.Person;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Extracts email address from person identified using it's displayed index from the list of students.
@@ -20,8 +19,8 @@ public class EmailCommand extends Command {
 
     public static final String COMMAND_WORD = "email";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Prepares for email to be sent to email address of " +
-            "the person identified by the index number used in the displayed person list.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Prepares for email to be sent to email address of "
+            + "the person identified by the index number used in the displayed person list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
