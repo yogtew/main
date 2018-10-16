b67b5b25659fd95bde16e991348d83d0ddc070fc
andrewtanJS <andrew.jinshen@gmail.com>
2018-10-16 22:28:14 +0800

    Add tests for event fields
    These tests ensure that the event fields are valid.
    Also, some of the event fields themselves were modified to ensure that the tests were correct and passing.

--- src/main/java/seedu/address/model/event/EventName.java
+++ src/main/java/seedu/address/model/event/EventName.java
@@ -15,7 +15,7 @@ public class EventName {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String EVENT_NAME_VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String EVENT_NAME_VALIDATION_REGEX = "[^\\s].*";
 
     public final String eventName;
 
