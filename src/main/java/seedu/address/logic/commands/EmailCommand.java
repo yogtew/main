a17b358810ede3109945449e583b0a6c8d96bd91
preethid <e0203237@u.nus.edu>
2018-10-26 22:01:54 +0800

    Edit email command to take in index, subject and body

--- src/main/java/seedu/address/logic/commands/EmailCommand.java
+++ src/main/java/seedu/address/logic/commands/EmailCommand.java
@@ -4,12 +4,12 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SUBJECT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_BODY;
 
-import seedu.address.model.email.*;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.email.EmailDraft;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Person;
 
