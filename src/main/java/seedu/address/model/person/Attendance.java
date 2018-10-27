f53562afdb39f1a5f29f9683134ec60701832bab
yogtew <reachyogusvi@gmail.com>
2018-10-27 11:40:59 +0800

    Fixed checkstyle errors

--- src/main/java/seedu/address/model/person/Attendance.java
+++ src/main/java/seedu/address/model/person/Attendance.java
@@ -2,6 +2,9 @@ package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Represents a Person's attendance
+ */
 public class Attendance {
     public final String value;
 
