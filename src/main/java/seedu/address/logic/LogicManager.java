8c8547481c73c2985713d0b23f1d3a0bdda6f50b
andrewtanJS <andrew.jinshen@gmail.com>
2018-10-30 00:37:23 +0800

    Add event list for the UI
    The event list mirrors the person list for the sake of consistency. It displays all the events and their respective fields in the same manner.

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -11,6 +11,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.AddressBookParser;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
+import seedu.address.model.event.Event;
 import seedu.address.model.person.Person;
 
 /**
@@ -43,6 +44,11 @@ public class LogicManager extends ComponentManager implements Logic {
     @Override
     public ObservableList<Person> getFilteredPersonList() {
         return model.getFilteredPersonList();
+    }
+
+    @Override
+    public ObservableList<Event> getFilteredEventList() {
+        return model.getFilteredEventList();
     }
 
     @Override
---
+++ src/main/java/seedu/address/ui/EventListPanel.java
@@ -0,0 +1,71 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import com.google.common.eventbus.Subscribe;
+
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.model.event.Event;
+
+/**
+ * Panel containing the list of events.
+ */
+public class EventListPanel extends UiPart<Region> {
+    private static final String FXML = "EventListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(EventListPanel.class);
+
+    @FXML
+    private ListView<Event> eventListView;
+
+    public EventListPanel(ObservableList<Event> eventList) {
+        super(FXML);
+        setConnections(eventList);
+        registerAsAnEventHandler(this);
+    }
+
+    private void setConnections(ObservableList<Event> eventList) {
+        eventListView.setItems(eventList);
+        eventListView.setCellFactory(listView -> new EventListViewCell());
+    }
+
+    /**
+     * Scrolls to the {@code EventCard} at the {@code index} and selects it.
+     */
+    private void scrollTo(int index) {
+        Platform.runLater(() -> {
+            eventListView.scrollTo(index);
+            eventListView.getSelectionModel().clearAndSelect(index);
+        });
+    }
+
+    @Subscribe
+    private void handleJumpToListRequestEvent(JumpToListRequestEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        scrollTo(event.targetIndex);
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Event} using a {@code EventCard}.
+     */
+    class EventListViewCell extends ListCell<Event> {
+        @Override
+        protected void updateItem(Event event, boolean empty) {
+            super.updateItem(event, empty);
+
+            if (empty || event == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new EventCard(event, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
