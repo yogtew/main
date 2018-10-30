b9b64aa983632f6ffb6f45747d29296e3f642764
alyip98 <alyip98@gmail.com>
2018-10-30 17:54:13 +0800

    Extended IMarkExecutable to include args

--- src/main/java/seedu/address/logic/commands/mark/IMarkExecutable.java
+++ src/main/java/seedu/address/logic/commands/mark/IMarkExecutable.java
@@ -7,5 +7,5 @@ import seedu.address.model.mark.Mark;
  * Interface for commands that support execution with marks
  */
 public interface IMarkExecutable {
-    public CommandResult executeMark(Mark m);
+    public CommandResult executeMark(Mark m, String[] args);
 }
