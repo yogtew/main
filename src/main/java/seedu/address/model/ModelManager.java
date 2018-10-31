3322d46550c4166c178cb98829b81c395512c30c
alyip98 <alyip98@gmail.com>
2018-11-01 04:41:43 +0800

    Style fix

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -10,6 +10,7 @@ import java.util.function.Predicate;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
