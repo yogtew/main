b4c23e5bedc6367a71bbae41382e2db3062c885f
yogtew <reachyogusvi@gmail.com>
2018-10-31 21:48:29 +0800

    Connect attendance field to PersonCard

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -49,6 +49,7 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
+        attendance.setText(person.getAttendance().value.toString());
         person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 
