89a69f324fe24c24bd6418089d50c5251b925ab7
alyip98 <alyip98@gmail.com>
2018-11-01 04:16:03 +0800

    Style fix

--- src/main/java/seedu/address/logic/commands/mark/MarkCommand.java
+++ src/main/java/seedu/address/logic/commands/mark/MarkCommand.java
@@ -25,17 +25,17 @@ public class MarkCommand extends Command {
     public static final String FIND = "find";
     public static final String JOIN = "join";
     public static final String AND = "and";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Creates a Mark.\n" +
-            "Parameters: " +
-            "[" + PREFIX_MARK + "MARK1] " +
-            "[" + SHOW + "|" + FIND + "|" + JOIN + "|" + AND + "] " +
-            "[" + PREFIX_MARK + "MARK2] " +
-            "[" + PREFIX_MARK + "MARK3]\n" +
-            "Example: mark " +
-            PREFIX_MARK + "mark1 " +
-            JOIN + " " +
-            PREFIX_MARK + "mark2 " +
-            PREFIX_MARK + "mark3";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Creates a Mark.\n"
+            + "Parameters: "
+            + "[" + PREFIX_MARK + "MARK1] "
+            + "[" + SHOW + "|" + FIND + "|" + JOIN + "|" + AND + "] "
+            + "[" + PREFIX_MARK + "MARK2] "
+            + "[" + PREFIX_MARK + "MARK3]\n"
+            + "Example: mark "
+            + PREFIX_MARK + "mark1 "
+            + JOIN + " "
+            + PREFIX_MARK + "mark2 "
+            + PREFIX_MARK + "mark3";
 
     protected String alias1;
     protected String alias2;
