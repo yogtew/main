b0f6eeef4cd9563368d532542911a805143a9ac8
preethid <e0203237@u.nus.edu>
2018-11-09 17:41:00 +0800

    add Email feature - incomplete and ongoing

--- src/main/java/seedu/address/logic/OutlookRequest.java
+++ src/main/java/seedu/address/logic/OutlookRequest.java
@@ -1,10 +1,22 @@
 package seedu.address.logic;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import java.nio.charset.StandardCharsets;
+import java.security.PrivilegedExceptionAction;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import com.microsoft.aad.adal4j.AuthenticationContext;
+import com.microsoft.aad.adal4j.AuthenticationResult;
+import net.minidev.json.JSONObject;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import javax.naming.ServiceUnavailableException;
 
 /**
  * Handles the sending of the mail
@@ -33,6 +45,8 @@ public class OutlookRequest {
         return subject;
     }
 
+    private final static String CLIENT_ID = "f6570890-f782-400d-a2a4-22374e313a33";
+
     /**
      * Connecting to outlook with appropriate tokens to send email
      */
@@ -40,28 +54,25 @@ public class OutlookRequest {
 
         try {
 
-            String url = "https://outlook.office.com/api/v2.0/me/sendmail";
+            String url = "https://outlook.office.com/api/v2.0/me/sendmail"; //here is definitely this url
 
             URL obj = new URL(url);
             HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+
+            //this is for the Auth-code grant method
+            String authCode = getAuthCode();
+            String accessToken = getAccessToken();
+
+            /*String email = "johndoe2103@outlook.com";
+            String password = "software2103";
+            AuthenticationResult result = getAccessTokenFromUserCredentials("https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize", email, password);
+            String accessToken = result.getAccessToken();
+            */
+
             conn.setRequestMethod("POST");
             conn.setDoOutput(true);
 
-            String basicAuth = "Bearer EwA4A+l3BAAUv0lYxoez7x2t6RowHa2liVeLW/wAAZv01ygjLaIsxm+"
-                    + "uty3OAuwW0rqZEKW1mTT5dDr/GuAx3zDGSrVl5o/CgN6BeS9U7mesRgnkUNsLwYRoVDcoz/hA2yvz204My"
-                    + "FnXo86xT4PmkbshXrpkl2cSkbyy8DxzQTO60K3qRXYsEI5Ypn0Kqvx4OC+hAo7QnWSrQcNkMPry5BpI3/50f8P4"
-                    + "GUu3G0E2tlsEEOQtRmaUu0udvJHonSXmGLIJN6zC7N2+LQ6w/SneP474D/Zn1ujGcpVDBMJWEns44V0S5/q5eKxgc1"
-                    + "zlnAFf6GbWYJMJ/VOVSHxGAG9BjZ4OlCENPA8K2oNj7jjzc8GCC9Su4hDdxBbYx4Z/yS4DZgAACDJWRudcrPxBCAI3"
-                    + "vNfEGZwb091B6iRzpToU+F+uc6EMqspQ5SmXHzgF0qYi1e1pSSRbnKWYjUd5zWnbXu46s9VT1EfmCZ0CM1NrIsw0lpmr"
-                    + "5KWHqAwJPXCuENsXJ8iVnFbdNix3JRKGqgP/4PXu8fd8ICO29JmoeDNKlmmSuB0iFXBzR75ETmLNd3mZ26sN0x91ICxc"
-                    + "xSu/fFwyZzx39byFg1VgTULVH4PHvwc1jSEUO8oV8T2fidb3WAwFoePWc/HyR7PRPa0r99uUtPK5BuXyUpE66Cy0RjL4o"
-                    + "B+GKw5LR4flKwPHuZAqUa0NjPqu1Wur8TmmQrJwqDNc9FjHNvum95muBtIUKrP7tmIB74uIwVYK2zk4gURyM3yqdjbuj"
-                    + "8+RvAVFykqhu/bNVhJMI7Beg3+xlkDHGB9qhHrCTolwZ7VfVR6+9P/nV2rOASt5pK9PBZ5wZSucAjjMQ04MhtokWJAA"
-                    + "7mKpn5F0pakxi4LnQLU90vwD68B+OrEBtCj9FSbOtD1vQCwU51HKtI1QW40kzEddsbfyzrDqw0+1iEDt0PyEuoVzit2"
-                    + "d5uiwL/ABxPQwEGY4g33wzgBceinG2clZeriOWAbaGAG7e6O99ZaeM2XcE3/5/236i7K9Wqq0/yDELDWklmQD2X8UFq"
-                    + "s/4C8fHjsuSU+8lAwQvfIuYA1FFE2LWX/tAK4Tazy5uF4N68vtMwI=";
-
-            conn.setRequestProperty ("Authorization", basicAuth);
+            conn.setRequestProperty ("Authorization", "Bearer" + accessToken);
             conn.setRequestProperty("Accept", "application/json; odata.metadata=none");
             conn.setRequestProperty("Content-Type", "application/json");
 
@@ -91,5 +102,179 @@ public class OutlookRequest {
         }
 
     }
+
+
+    public static String getAuthCode() {
+        try {
+            String authEndpoint = "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize";
+            URL obj = new URL(authEndpoint);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            //connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            String urlParameters = "client_id="
+                    + CLIENT_ID
+                    + "&response_type=code"
+                    + "&redirect_uri=https://login.microsoftonline.com/common/oauth2/nativeclient"
+                    + "&response_mode=form_post"
+                    + "&scope=https://graph.microsoft.com/mail.send"
+                    + "&state=12345";
+            byte[] outputInBytes = urlParameters.getBytes("UTF-8");
+            OutputStream os = connection.getOutputStream();
+            os.write(outputInBytes);
+            os.close();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String inputLine;
+            String[] array = new String[100];
+            int i = 0;
+            while((inputLine =in.readLine()) != null){
+                array[i] = inputLine;
+                i++;
+            }
+           /* String[] sb = new String[];
+            for (int c; (c = in.read()) >= 0;)
+                sb.add((char)c);
+            String authCode = sb.toString();
+           */
+           in.close();
+            String authCode = array[0];
+            System.out.println(authCode);
+            return authCode;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public static String getAccessToken() {
+
+        String testEmail = "johndoe2103@outlook.com";
+        String testPassword = "software123";
+        String urlParams = "username=" + testEmail + "&password=" + testPassword + "&grant_type=password"
+                + "&scope=openid" + "&client_id=" + CLIENT_ID + "&resource=https://login.microsoftonline.com/organization/oauth2/nativeclient";
+
+        byte[] postData = urlParams.getBytes(StandardCharsets.UTF_8);
+        int postDataLength = postData.length;
+
+        String accessToken = null;
+        try {
+            String tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+            URL obj = new URL(tokenEndpoint);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("charset", "utf-8");
+            connection.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+            connection.setUseCaches(false);
+
+            /*connection.setRequestProperty("client_id", CLIENT_ID);
+            connection.setRequestProperty("scope","https://outlook.office.com/mail.send");
+            connection.setRequestProperty("redirect_uri", "https://login.microsoftonline.com/common/oauth2/nativeclient");
+            connection.setRequestProperty("code", authCode);
+            connection.setRequestProperty("grant_type", "authorization_code");
+            connection.setRequestMethod("POST");
+            */
+
+            /*String urlParameters = "client_id="
+                    + CLIENT_ID
+                    + "&grant_type=password"
+                    + "&scope=https://outlook.office.com/openid"
+                    + ""
+            */
+
+
+            try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+                wr.write(postData);
+            }
+
+            int responseCode = connection.getResponseCode();
+            String responseMsg = connection.getResponseMessage();
+            if (responseCode >= 400 && responseCode <= 499) {
+                throw new Exception(responseMsg + " :: " + responseCode);
+            }
+
+            InputStream in = connection.getInputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            StringBuffer response = new StringBuffer();
+
+            String inputLine;
+
+            while ((inputLine = reader.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            accessToken = response.toString();
+
+            // JSONObject myResponse = new JSONObject(response);
+
+
+
+
+
+            /*BufferedReader in = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            System.out.println(content.toString());
+            return content.toString();
+
+            /*String inputLine;
+            String[] array = new String[100];
+            int i = 0;
+            while((inputLine = in.readLine()) != null){
+                array[i] = inputLine;
+                i++;
+            }
+            in.close();
+            String accessToken = array[0];
+            System.out.println(accessToken);
+            return accessToken;
+            */
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return accessToken;
+    }
+
+
+    /*private static AuthenticationResult getAccessTokenFromUserCredentials(String url, String email, String password) throws Exception {
+        AuthenticationContext context;
+        AuthenticationResult result;
+        ExecutorService service = null;
+        try {
+            service = Executors.newFixedThreadPool(1);
+            context = new AuthenticationContext(url, false, service);
+            Future<AuthenticationResult> future = context.acquireToken(
+                    "https://graph.windows.net", CLIENT_ID, email, password,
+                    null);
+            result = future.get();
+        } finally {
+            service.shutdown();
+        }
+
+        if (result == null) {
+            throw new ServiceUnavailableException(
+                    "authentication result was null");
+        }
+        return result;
+    }
+    */
+
+
 
 }
