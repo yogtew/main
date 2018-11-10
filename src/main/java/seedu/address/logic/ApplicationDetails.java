dee7f2c1cbe7ae91af48bc8baa2d37ed3f39f340
preethid <e0203237@u.nus.edu>
2018-11-10 12:28:51 +0800

    add Email feature functional, not fully clean

--- src/main/java/seedu/address/logic/ApplicationDetails.java
+++ src/main/java/seedu/address/logic/ApplicationDetails.java
@@ -4,6 +4,15 @@ package seedu.address.logic;
  * Details on application registration under Azure, and necessary endpoints
  */
 public class ApplicationDetails {
+
+    private static String appId = "ba267106-3b1d-4c68-895e-70353f8b4843";
+    private static String authUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=ba267106"
+            + "-3b1d-4c68-895e-70353f8b4843&scope=Mail.send&response_type=code&response_mode=form_post&redirect_uri="
+            + "http%3A%2F%2Flocalhost%3A8000%2Fcontact&prompt=consent";
+    private static String mailEndpoint = "https://graph.microsoft.com/v1.0/me/sendMail";
+    private static String redirectUri = "http%3A%2F%2Flocalhost%3A8000%2Fcontact";
+    private static String tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+
     public static String getAppId() {
         return appId;
     }
@@ -23,14 +32,6 @@ public class ApplicationDetails {
     public static String getRedirectUri() {
         return redirectUri;
     }
-
-    private static String appId = "ba267106-3b1d-4c68-895e-70353f8b4843";
-    private static String authUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=ba267106"
-            + "-3b1d-4c68-895e-70353f8b4843&scope=Mail.send&response_type=code&response_mode=form_post&redirect_uri="
-            + "http%3A%2F%2Flocalhost%3A8000%2Fcontact&prompt=consent";
-    private static String mailEndpoint = "https://graph.microsoft.com/v1.0/me/sendMail";
-    private static String redirectUri = "http%3A%2F%2Flocalhost%3A8000%2Fcontact";
-    private static String tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
 
 }
 
