bc3879997da1ca86d3578cf1f059bdb1c3f32370
preethid <e0203237@u.nus.edu>
2018-11-10 21:31:38 +0800

    add Email feature functional, not fully clean

--- src/main/java/seedu/address/logic/ApplicationDetails.java
+++ src/main/java/seedu/address/logic/ApplicationDetails.java
@@ -5,14 +5,13 @@ package seedu.address.logic;
  */
 public class ApplicationDetails {
 
-    public static final String appId = "ba267106-3b1d-4c68-895e-70353f8b4843";
-    public static final String authUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=" +
-            appId + "&scope=Mail.send&response_type=code&response_mode=form_post&redirect_uri="
+    public static final String APPID = "ba267106-3b1d-4c68-895e-70353f8b4843";
+    public static final String AUTHURL = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id="
+            + APPID + "&scope=Mail.send&response_type=code&response_mode=form_post&redirect_uri="
             + "http%3A%2F%2Flocalhost%3A8000%2Fcontact&prompt=consent";
-    public static final String mailEndpoint = "https://graph.microsoft.com/v1.0/me/sendMail";
-    public static final String redirectUri = "http%3A%2F%2Flocalhost%3A8000%2Fcontact";
-    public static final String tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
-
+    public static final String MAILENDPOINT = "https://graph.microsoft.com/v1.0/me/sendMail";
+    public static final String REDIRECTURL = "http%3A%2F%2Flocalhost%3A8000%2Fcontact";
+    public static final String TOKENENDPOINT = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
 
 }
 
--- src/main/java/seedu/address/logic/OutlookRequest.java
+++ src/main/java/seedu/address/logic/OutlookRequest.java
@@ -45,7 +45,7 @@ public class OutlookRequest {
 
         try {
             // Opens microsoft authentication page in a browser tab
-            Desktop.getDesktop().browse(new URI(ApplicationDetails.authUrl));
+            Desktop.getDesktop().browse(new URI(ApplicationDetails.AUTHURL));
 
             // setup a http listener on localhost:8000 to wait for the returned authcode
             ServerSocket serverSocket = new ServerSocket(8000);
@@ -67,12 +67,12 @@ public class OutlookRequest {
                             + "&scope=mail.send"
                             + "&code=%s"
                             + "&redirect_uri=%s",
-                    ApplicationDetails.appId,
+                    ApplicationDetails.APPID,
                     authCode,
-                    ApplicationDetails.redirectUri);
+                    ApplicationDetails.REDIRECTURL);
 
             HttpURLConnection tokenRequest = seedu.address.logic.Request.tokenRequest(
-                    ApplicationDetails.tokenEndpoint, "POST", tokenParams);
+                    ApplicationDetails.TOKENENDPOINT, "POST", tokenParams);
             List<String> response = seedu.address.logic.Request.read(tokenRequest);
 
             String s = response.get(0);
@@ -95,7 +95,7 @@ public class OutlookRequest {
                     + emailAdd + "\"}}]},\"SaveToSentItems\":\"true\"}";
 
 
-            HttpURLConnection emailRequest = Request.sendMail(token, ApplicationDetails.mailEndpoint,
+            HttpURLConnection emailRequest = Request.sendMail(token, ApplicationDetails.MAILENDPOINT,
                     "POST", emailBody);
             emailRequest.connect();
             int responseCode = emailRequest.getResponseCode();
