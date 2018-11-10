cb1d9ac8065ea356187f4127f8e82c982bfa2d64
cyrusguo <cyrus224@gmail.com>
2018-11-10 19:43:38 +0800

    add Graph Panel for display of attendance graph

---
+++ src/main/java/seedu/address/ui/GraphPanel.java
@@ -0,0 +1,72 @@
+package seedu.address.ui;
+
+import java.net.URL;
+import java.util.logging.Logger;
+
+import com.google.common.eventbus.Subscribe;
+
+import javafx.application.Platform;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Region;
+import javafx.scene.web.WebView;
+import seedu.address.MainApp;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.StudentPanelSelectionChangedEvent;
+import seedu.address.model.student.Student;
+
+/**
+ * The Browser Panel of the App.
+ */
+public class GraphPanel extends UiPart<Region> {
+
+    public static final String DEFAULT_PAGE = "default.html";
+    public static final String SEARCH_PAGE_URL =
+            "https://se-edu.github.io/addressbook-level4/DummySearchPage.html?name=";
+
+    private static final String FXML = "BrowserPanel.fxml";
+
+    private final Logger logger = LogsCenter.getLogger(getClass());
+
+    @FXML
+    private WebView browser;
+
+    public GraphPanel() {
+        super(FXML);
+
+        // To prevent triggering events for typing inside the loaded Web page.
+        getRoot().setOnKeyPressed(Event::consume);
+
+        loadDefaultPage();
+        registerAsAnEventHandler(this);
+    }
+
+    private void loadPersonPage(Student student) {
+        loadPage(SEARCH_PAGE_URL + student.getName().fullName);
+    }
+
+    public void loadPage(String url) {
+        Platform.runLater(() -> browser.getEngine().load(url));
+    }
+
+    /**
+     * Loads a default HTML file with a background that matches the general theme.
+     */
+    private void loadDefaultPage() {
+        URL defaultPage = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
+        loadPage(defaultPage.toExternalForm());
+    }
+
+    /**
+     * Frees resources allocated to the browser.
+     */
+    public void freeResources() {
+        browser = null;
+    }
+
+    @Subscribe
+    private void handleStudentPanelSelectionChangedEvent(StudentPanelSelectionChangedEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        loadPersonPage(event.getNewSelection());
+    }
+}
